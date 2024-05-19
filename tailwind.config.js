ec42fbaea02aa583edb9b68772180465f7b22a0d
Adhitya Aditama Nugraha <aaditamanugraha@Adhityas-MacBook-Air.local>
2024-05-19 23:06:17 +0700

    feat: set up styling guide to input tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      green: "hsl(75, 94%, 57%)",
+      white: "hsl(0, 0%, 100%)",
+      grey: "hsl(0, 0%, 20%)",
+      "dark-grey": "hsl(0, 0%, 12%)",
+      "off-black": "hsl(0, 0%, 8%)",
+    },
+    fontFamily: {
+      inter: ["Inter", "sans-serif"],
+    },
   },
   plugins: [],
 };
